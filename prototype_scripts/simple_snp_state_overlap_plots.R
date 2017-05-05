4a19578b1ea08043e594f80a6a62ed0bb3d43de3
Eivind Gard Lund <e.g.lund@medisin.uio.no>
2017-05-05 23:17:15 +0200

    plot that identifies T-cell specific regions

--- prototype_scripts/simple_snp_state_overlap_plots.R
+++ prototype_scripts/simple_snp_state_overlap_plots.R
@@ -25,27 +25,60 @@ dir.create('out/prototype_scripts/plots', recursive = TRUE, showWarnings = FALSE
 ggsave('out/prototype_scripts/plots/number_of_risk_loci_covered_by_enhancers.png')
 
 
-edf <- snps_weighted_per_state %>% 
-  filter(str_detect(state, 'EnhG?$')) %>%
-  group_by(eid, tag_snp, n_proxies) %>% 
-  summarise(n_overlapping = sum(n_overlapping))
+edf <- local({
+  x <- snps_weighted_per_state %>% 
+    filter(str_detect(state, 'EnhG?$')) %>%
+    group_by(eid, tag_snp, n_proxies) %>% 
+    summarise(n_overlapping = sum(n_overlapping))
+  x <- sample_meta %>% 
+    distinct(eid, group) %>% 
+    inner_join(x, by = 'eid')
+  # select max number of SNPs overlapped by any track per tag_snp
+  best_overlap <- x %>% 
+    group_by(tag_snp, group) %>% 
+    summarise(n_overlapping = median(n_overlapping)) %>% 
+    summarize(max_overlap = max(n_overlapping))
+  x %>% 
+    inner_join(best_overlap, by = 'tag_snp') %>% 
+    mutate(score = n_overlapping / max_overlap,
+           group = factor(group, levels = group_order)) %>% 
+    dplyr::select(-max_overlap)
+})
 
-# select max number of SNPs overlapped by any track per tag_snp
-best_overlap <- edf %>% 
-  group_by(tag_snp) %>% 
-  summarize(max_overlap = max(n_overlapping))
 
-edf <- sample_meta %>% 
-  distinct(eid, group) %>% 
-  inner_join(edf, by = 'eid') %>% 
-  inner_join(best_overlap, by = 'tag_snp')
 edf %>% 
-  mutate(group = factor(group, levels = group_order)) %>% 
-  group_by(group, tag_snp, n_proxies, max_overlap) %>% 
-  summarise(n_overlapping = median(n_overlapping)) %>% 
-  mutate(overlap_score = n_overlapping / max_overlap) %>% 
+  group_by(group, tag_snp) %>% 
+  summarise(score = median(score)) %>% 
   ggplot() +
-  geom_tile(aes(tag_snp, group, fill = overlap_score, color = 'white')) +
+  geom_tile(aes(tag_snp, group, fill = score, color = 'white')) +
   scale_fill_gradient(low = 'white', high = 'steelblue') +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 ggsave('out/prototype_scripts/plots/tag_snps_scored_by_category.png')
+ggsave('out/prototype_scripts/plots/tag_snps_scored_by_category.pdf') # pdf so I can copy SNP ids
+
+
+# One group has score 1.0 per tag SNP.
+# Drop Blood & T-cell group and select SNPs with max score below e.g. 0.5 (because then T-cell had 1.0)
+tag_snps <- read_tsv('input_data/external_static/CeD_tag_SNPs.bed', col_names = c('chrom', 'start', 'end', 'tag_snp'))
+#except T-cells
+top_group_per_tag_snp <- edf %>% 
+  group_by(group, tag_snp) %>% 
+  summarise(score = median(score)) %>% 
+  filter(!str_detect(group, 'T-cell')) %>% 
+#  filter(!str_detect(group, 'Thymus')) %>% # did not improve overlap with ASE effects
+  ungroup() %>% 
+  group_by(tag_snp) %>% 
+  slice(which.max(score)) %>% 
+  ungroup()
+
+tcell_snps <- top_group_per_tag_snp %>% 
+  filter(score < 0.6) %>% 
+  inner_join(tag_snps, by = 'tag_snp') %>% 
+  dplyr::select(chrom, start, end, everything())
+tcell_snps
+tcell_snps %>% 
+  write_tsv('out/prototype_scripts/T-cell_specific_tag_snps.bed', col_names = FALSE)
+tcell_ase <- read_csv('input_data/external_static/potential_ASE_by_tag_snp.csv')
+tcell_ase %>% 
+  inner_join(tcell_snps)
+tcell_snps
